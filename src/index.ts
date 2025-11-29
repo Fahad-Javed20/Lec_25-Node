5488822375ea7add62a080131a28004189571d14
Fahad Bin Javed <126807388+Fahad-Javed20@users.noreply.github.com>
2025-11-29 14:58:51 +0500

    make server

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,41 @@
-import os from "os";
-console.log( `cpu cores : ${os.cpus().lenght}`);
+import http from "http"
 
-console.log("fahad bin javed")
-console.log("fahad bin javed")
+const users = [
+    {id:1,name:"Fahad",salary:'4M'},
+    {id:2,name:"Fahad",salary:'4M'},
+    {id:2,name:"Fahad",salary:'4M'}
+]
+
+const server = http.createServer((req,res)=>{
+    console.log("request Initated")
+    res.end("Fity Mun")
+    const {method,url,headers} = req
+
+
+
+if(url === "/users" && method ==="GET"){
+    res.statusCode = 200;
+    res.setHeader("Content-Type","application/json")
+    return res.end(JSON.stringify(users));
+}
+});
+
+server.listen(3002,()=>{
+    console.log("server is running")
+});
+
+
+
+
+
+
+// import os from "os";
+// import fs from "fs"
+// console.log( `cpu cores : ${os.cpus().length}`);
+
+
+// console.log("Stat")
+// // const data = fs.readFileSync("C:/Users/S.M/Desktop/Lec_25-Node/sample.tsx","utf-8")
+// fs.appendFileSync("new_file.txt", `created new file`)
+// // console.log(data)
+// console.log("Read Successfully")
