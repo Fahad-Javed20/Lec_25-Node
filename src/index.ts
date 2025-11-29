52b67670c5ee4b51f700d264297d5e5925c7c04d
Fahad Bin Javed <126807388+Fahad-Javed20@users.noreply.github.com>
2025-11-29 15:14:13 +0500

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,38 +1,33 @@
-import http from "http"
+import http from "http";
 
 const users = [
-    {id:1,name:"Fahad",salary:'4M'},
-    {id:2,name:"Fahad",salary:'4M'},
-    {id:2,name:"Fahad",salary:'4M'}
-]
+  { id: 1, name: "Fahad", salary: "4M" },
+  { id: 2, name: "Fahad", salary: "4M" },
+  { id: 3, name: "Fahad", salary: "4M" },
+];
 
-const server = http.createServer((req,res)=>{
-    console.log("request Initated")
-    res.end("Fity Mun")
-    const {method,url,headers} = req
+const server = http.createServer((req, res) => {
+  const { method, url } = req;
 
+  console.log("request Initiated");
 
-
-if(url === "/users" && method ==="GET"){
+  if (url === "/users" && method === "GET") {
     res.statusCode = 200;
-    res.setHeader("Content-Type","application/json")
+    res.setHeader("Content-Type", "application/json");
     return res.end(JSON.stringify(users));
-}
+  }
+
+  res.statusCode = 200;
+  res.end("Fity Mun");
 });
 
-server.listen(3002,()=>{
-    console.log("server is running")
+server.listen(3002, () => {
+  console.log("server is running");
 });
-
-
-
-
-
 
 // import os from "os";
 // import fs from "fs"
 // console.log( `cpu cores : ${os.cpus().length}`);
-
 
 // console.log("Stat")
 // // const data = fs.readFileSync("C:/Users/S.M/Desktop/Lec_25-Node/sample.tsx","utf-8")
